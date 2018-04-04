17241da9f91b110d3c6bd5dc6160ec700feb8a19
CodeDad <github email address>
2018-04-04 15:02:09 -0400

    added create, show and strong params to the songs_controller

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -4,5 +4,25 @@ class Api::SongsController < ApplicationController
     render json: Song.all
   end
 
+  def create 
+    song = Song.new(song_params)
+    if song.save 
+      render json:song
+    else
+      render json: {message: song.errors}, status: 400 
+    end
+  end
+
+
+  def show
+    render json: Song.find_by(id: params[:id])
+  end
+
+
+  private
+    def song_params
+      params.require(:song).permit(:name, :lyrics, :chords, :chord_chart)
+    end
+
   
 end
