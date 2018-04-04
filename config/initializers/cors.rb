f3e69d34cdf459c955c8931db5a0a045eb2b578f
CodeDad <github email address>
2018-04-04 13:47:54 -0400

    created routes with a namespace for api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000/'
 
+    # to add heroku url to this - 
+    # origins ['http://localhost:3000/', 'https://dashboard.heroku.com/apps/app-url' ]
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
